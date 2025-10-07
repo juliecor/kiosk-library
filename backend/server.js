07bfe478951cb21e3aeb040b16d082c6487d0177
juliecor <juliecor>
2025-10-07 11:04:18 +0800

    add a new routes for reports

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const bookRoutes = require("./routes/bookRoutes");
 const studentRoutes = require("./routes/studentRoutes");
 const borrowRoutes = require("./routes/borrowRoutes");
 const statsRoutes = require("./routes/statsRoutes");
+const reportsRoutes = require("./routes/reportsRoutes");
+
 const cors = require("cors");
 
 dotenv.config();
@@ -27,6 +29,7 @@ app.use("/api/books", bookRoutes);
 app.use("/api/students", studentRoutes);
 app.use("/api/borrow", borrowRoutes);
 app.use("/api/stats", statsRoutes);
+app.use("/api/reports", reportsRoutes);
 
 const PORT = process.env.PORT || 5000;
 
